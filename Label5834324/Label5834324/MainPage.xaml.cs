a4128034c7b92e3fd1113a0f8b7b4ea5fd6bc299
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-07-22 10:43:40 -0600

    Agregar archivos de proyecto.

---
+++ Label5834324/Label5834324/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace Label5834324
+{
+    public partial class MainPage : ContentPage
+    {
+        ///<Summary>
+        ///<Createddate>21/07/2023</createddate>
+        ///<company>INDEL</company>
+        ///<lastmodificationdate>21/07/2023</lastmodificationdate>
+        ///<lastmodificationdescription>
+        ///PROPIEDADES DEL LABEL
+        ///</lastmodificationdescription>
+        //////<lastmodifierautor>ELISEO</lastmodifierautor>
+        ///</Summary>
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
